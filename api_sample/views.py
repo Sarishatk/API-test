6f4644a5ffcfabeb70e68e367574adff842aeb9d
Sarisha.tk <sarishakrishna58@gmail.com>
2025-12-12 09:58:13 +0530

    er

--- api_sample/views.py
+++ api_sample/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from api_sample.models import StudentModel
 # Create your views here.
+
+
+
+class StudentListCreateView(APIView):
+
+    def get(self, request):
+
+        student_details = StudentModel.objects.all()
+
